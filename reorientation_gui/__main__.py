9ab497cc73b7cfa49e39e5b37f21efeecbc04b8e
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2024-05-13 09:36:45 +0200

    allow specifying an initial file in as cmd arg

--- reorientation_gui/__main__.py
+++ reorientation_gui/__main__.py
@@ -1,9 +1,19 @@
+import argparse
+
 from reorientation_gui.app import App
 from reorientation_gui.state import AppState
 from reorientation_gui.widgets.file_dialog import FileDialog
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str)
+args = parser.parse_args()
+
 app_state = AppState()
+if args.file is not None:
+    app_state.file_image_state_spect.filename.value = args.file
+
 app = App(app_state)
 # open file dialog after startup
-app.after(50, lambda *args: FileDialog(app_state).grab_set())
+if args.file is None:
+    app.after(50, lambda *args: FileDialog(app_state).grab_set())
 app.mainloop()
